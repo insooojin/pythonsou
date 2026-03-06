4cae0577a227932282804835601c5841e53a7899
soojin <sujinaa159@gmail.com>
2026-03-06 16:05:23 +0900

    .

---
+++ pro2network/pack2http/httpserver1.py
@@ -0,0 +1,12 @@
+# 단순한 HTTPServer 구축 - 기본적인 socket 연결 관리
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 7777
+
+handler = SimpleHTTPRequestHandler          # get 요청에 대해 문서를 읽어 클라이언트로 전송하는 역할
+
+# HTTPServer 객체 생성
+serv = HTTPServer(('127.0.0.1', PORT), handler) # http생성자
+print('웹 서비스 시작...')
+serv.serve_forever()            # 웹서비스 무한루핑
